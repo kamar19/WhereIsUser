32cd14f4881794e14a81df017337d22a30c9b0b8
kamar19 <kipisov@mail.ru>
2021-03-10 09:47:52 +0600

    refactor code for splash logo

--- app/src/main/java/ru/firstset/whereisuser/MainActivity.java
+++ app/src/main/java/ru/firstset/whereisuser/MainActivity.java
@@ -20,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.Theme_WhereIsUser);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
